91468b30ac29f8cbccd79878fd97cf0b3460be95
urraka <perroazul64@gmail.com>
2014-02-27 19:43:21 -0300

    Changed net;Max\MinNickLength to size_t to remove signed\unsigned comparison warnings.

--- src/g13/net/constants.h
+++ src/g13/net/constants.h
@@ -8,13 +8,14 @@ namespace g13
 	{
 		static const int MaxPlayers         = 32;
 		static const int MaxHealth          = UINT16_MAX;
-		static const int MaxNickLength      = 20;
-		static const int MinNickLength      = 1;
 		static const int MaxTickOffset      = 63;
 		static const int InterpolationTicks = 4;
 		static const int TimeoutTicks       = 200;
 		static const int CountdownTicks     = 40;
 		static const int ChatTime           = 6000; // ms
+
+		static const size_t MaxNickLength      = 20;
+		static const size_t MinNickLength      = 1;
 
 		enum Channels
 		{
